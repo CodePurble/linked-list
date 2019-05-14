cb7a199dc80ff02d9f99e8d401a37d844b3360a5
CodePurble <ramprakash.chandrashekar@gmail.com>
2019-05-14 11:13:54 +0530

    Updated setup

--- setup.sh
+++ setup.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-cp include/* $1;
-cp src/* $2;
-
 if ( $1 == test); then
     make makeDir && make;
     bin/test.out
+else
+    INC=$1;
+    SRC=$2;
+
+    if [ ! -d $INC ]; then
+        mkdir $INC
+    elif [ ! -d $SRC]; then
+        mkdir $SRC
+    else
+        mkdir $INC
+        mkdir $SRC
+    fi
+    
+    cp src/* $SRC;
+    cp include/* $INC;
 fi
